00035dab15b87f46476e818365a2603c02d53c23
yuanwei <yuanwei@kexinjinke.com>
2020-11-27 21:30:25 +0800

    临时提交

--- happymall-mapper/src/main/java/cn/coderap/mapper/ItemsMapperCustom.java
+++ happymall-mapper/src/main/java/cn/coderap/mapper/ItemsMapperCustom.java
@@ -2,6 +2,7 @@ package cn.coderap.mapper;
 
 import cn.coderap.pojo.vo.ItemCommentVO;
 import cn.coderap.pojo.vo.SearchItemsVO;
+import cn.coderap.pojo.vo.ShopcartItemVO;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
@@ -18,4 +19,6 @@ public interface ItemsMapperCustom {
     public List<SearchItemsVO> searchItems(@Param("paramsMap") Map<String,Object> map);
 
     public List<SearchItemsVO> searchItemsByThirdCat(@Param("paramsMap") Map<String,Object> map);
+
+    public List<ShopcartItemVO> queryItemsBySpecIds(@Param("paramsList") List<String> list);
 }
