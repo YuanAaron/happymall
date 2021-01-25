1b6b736e8c5b1ccb44225a585c1cedafa1a0a295
yuanwei <yuanwei@kexinjinke.com>
2021-01-25 15:38:23 +0800

    个人中心-使用Hibernate验证BO用户信息

--- happymall-api/src/main/java/cn/coderap/controller/center/CenterUserController.java
+++ happymall-api/src/main/java/cn/coderap/controller/center/CenterUserController.java
@@ -10,10 +10,16 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by yw
@@ -33,9 +39,16 @@ public class CenterUserController {
             @ApiParam(name = "userId",value = "用户id",required = true)
             @RequestParam String userId,
             @ApiParam(name = "centerUserBO",value = "用户传入的数据封装在此entity中",required = true)
-            @RequestBody CenterUserBO centerUserBO,
+            @RequestBody @Valid CenterUserBO centerUserBO,
+            BindingResult result,
             HttpServletRequest request,
             HttpServletResponse response) {
+
+        //判断result中是否包含错误的验证信息，如果有，则直接return
+        if (result.hasErrors()) {
+            Map<String, String> errorMap = getErrors(result);
+            return JSONResult.ok(errorMap);
+        }
 
         Users userRes = centerUserService.updateUserInfo(userId, centerUserBO);
         //更新Users后，覆盖cookie
@@ -45,6 +58,20 @@ public class CenterUserController {
         //TODO 在分布式会话中，增加令牌token，整合进redis
 
         return JSONResult.ok();
+    }
+
+    //处理错误信息
+    private Map<String, String> getErrors(BindingResult result) {
+        Map<String, String> map = new HashMap<>();
+        List<FieldError> fieldErrorList = result.getFieldErrors();
+        for (FieldError fieldError : fieldErrorList) {
+            //发生验证错误的属性
+            String errorField = fieldError.getField();
+            //验证错误信息
+            String errorMessage = fieldError.getDefaultMessage();
+            map.put(errorField, errorMessage);
+        }
+        return map;
     }
 
     private Users setNullProperty(Users userRes) {
--- happymall-pojo/src/main/java/cn/coderap/pojo/bo/center/CenterUserBO.java
+++ happymall-pojo/src/main/java/cn/coderap/pojo/bo/center/CenterUserBO.java
@@ -2,12 +2,35 @@ package cn.coderap.pojo.bo.center;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.*;
 import java.util.Date;
 
 /**
  * Created by yw
  * 2021/1/25
+ *
+ * Bean Validation 中内置的 constraint：
+ * @Null 被注释的元素必须为 null
+ * @NotNull 被注释的元素必须不为 null
+ * @AssertTrue 被注释的元素必须为 true
+ * @AssertFalse 被注释的元素必须为 false
+ * @Min(value) 被注释的元素必须是一个数字，其值必须大于等于指定的最小值
+ * @Max(value) 被注释的元素必须是一个数字，其值必须小于等于指定的最大值
+ * @DecimalMin(value) 被注释的元素必须是一个数字，其值必须大于等于指定的最小值
+ * @DecimalMax(value) 被注释的元素必须是一个数字，其值必须小于等于指定的最大值
+ * @Size(max=, min=) 被注释的元素的大小必须在指定的范围内
+ * @Digits (integer, fraction) 被注释的元素必须是一个数字，其值必须在可接受的范围内
+ * @Past 被注释的元素必须是一个过去的日期
+ * @Future 被注释的元素必须是一个将来的日期
+ * @Pattern(regex=,flag=) 被注释的元素必须符合指定的正则表达式
+ * Hibernate Validator 附加的 constraint
+ * @NotBlank(message =) 验证字符串非null，且长度必须大于0
+ * @Email 被注释的元素必须是电子邮箱地址
+ * @Length(min=,max=) 被注释的字符串的大小必须在指定的范围内
+ * @NotEmpty 被注释的字符串必须非空
+ * @Range(min=,max=,message=) 被注释的元素必须在合适的范围内
  */
 @ApiModel(value="用户对象", description="从客户端，由用户传入的数据封装在此entity中")
 public class CenterUserBO {
@@ -23,25 +46,28 @@ public class CenterUserBO {
     @ApiModelProperty(value = "确认密码", name = "confirmPassword", example = "123456", required = false)
     private String confirmPassword;
 
-//    @NotBlank(message = "用户昵称不能为空")
-//    @Length(max = 12, message = "用户昵称不能超过12位")
+    @NotBlank(message = "用户昵称不能为空")
+    @Length(max = 12, message = "用户昵称不能超过12位")
     @ApiModelProperty(value = "用户昵称", name = "nickname", example = "杰森", required = false)
     private String nickname;
 
-//    @Length(max = 12, message = "用户真实姓名不能超过12位")
+    @Length(max = 12, message = "用户真实姓名不能超过12位")
     @ApiModelProperty(value = "真实姓名", name = "realname", example = "杰森", required = false)
     private String realname;
 
-//    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$", message = "手机号格式不正确")
+    @Pattern(regexp = "^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\\d{8})$", message = "手机号格式不正确")
     @ApiModelProperty(value = "手机号", name = "mobile", example = "13999999999", required = false)
     private String mobile;
 
-//    @Email
+    //@Pattern和@Email选其一即可
+    @Pattern(regexp = "^(\\w-*\\.*)+@(\\w-?)+(\\.\\w{2,})+$", message = "邮箱格式不正确")
+    //@Email
     @ApiModelProperty(value = "邮箱地址", name = "email", example = "imooc@imooc.com", required = false)
     private String email;
 
-//    @Min(value = 0, message = "性别选择不正确")
-//    @Max(value = 2, message = "性别选择不正确")
+    //测试发现：0.6会被转成0，1.6会被转成1，2.6会被转成2
+    @Min(value = 0, message = "性别选择不正确")
+    @Max(value = 2, message = "性别选择不正确")
     @ApiModelProperty(value = "性别", name = "sex", example = "0:女 1:男 2:保密", required = false)
     private Integer sex;
 
