b6cbab89bc7464d3dfb3495c8e4492d3a1c1d35f
yuanwei <yuanwei@kexinjinke.com>
2021-03-09 10:12:09 +0800

    使用spring session实现用户的分布式会话

--- happymall-api/src/main/java/cn/coderap/Application.java
+++ happymall-api/src/main/java/cn/coderap/Application.java
@@ -2,19 +2,23 @@ package cn.coderap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import tk.mybatis.spring.annotation.MapperScan;
 
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) //排除spring security的自动装配类（容器启动时有一些类被加载）,避免出现spring security的登录界面
 //扫描通用mapper所在的包
 @MapperScan(basePackages = "cn.coderap.mapper")
 //除了默认扫描cn.coderap外，增加扫描全局唯一id相关组件包
 @ComponentScan(basePackages = {"cn.coderap","org.n3r.idworker"})
 //开启定时任务
 @EnableScheduling
+//开启使用redis作为spring session
+@EnableRedisHttpSession
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
--- happymall-api/src/main/java/cn/coderap/controller/HelloController.java
+++ happymall-api/src/main/java/cn/coderap/controller/HelloController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @ApiIgnore
 @RestController
 public class HelloController {
@@ -19,5 +22,21 @@ public class HelloController {
         logger.warn("warn: hello");
         logger.error("error: hello");
         return "hello world";
+    }
+
+    /**
+     * 使用redis实现用户的分布式会话和使用Spring Session实现用户的分布式会话:
+     * 1、推荐前者
+     * 2、spring session与spring耦合，如果有其他模块用C++、Go等开发，想要获取spring session比较复杂
+     */
+
+    @GetMapping("/setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600); //设置过期时间
+        session.getAttribute("userInfo");
+//        session.removeAttribute("userInfo");
+        return "ok";
     }
 }
