df0460df3601d3773f10ee3dabebde62cb03fd30
yuanwei <yuanwei@kexinjinke.com>
2021-03-18 22:35:10 +0800

    跳转至统一登录页面

--- happymall-sso/src/main/java/cn/coderap/controller/HelloController.java
+++ happymall-sso/src/main/java/cn/coderap/controller/HelloController.java
@@ -3,6 +3,7 @@ package cn.coderap.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
@@ -17,9 +19,15 @@ public class HelloController {
 
     private final static Logger logger= LoggerFactory.getLogger(HelloController.class);
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "hello world";
+    @GetMapping("/login")
+    public String login(String returnUrl,
+                        Model model,
+                        HttpServletRequest request,
+                        HttpServletResponse response) {
+        model.addAttribute("returnUrl",returnUrl);
+        //TODO 后续完善是否登录校验
+
+        //如果用户从未登录过，第一次访问则跳转到CAS的统一登录页面
+        return "login";
     }
 }
