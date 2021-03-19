0ce4e58460b70e5a6d319b94b0d5a6122d113a17
yuanwei <yuanwei@kexinjinke.com>
2021-03-19 14:23:46 +0800

    创建全局门票、临时门票

--- happymall-sso/src/main/java/cn/coderap/controller/HelloController.java
+++ happymall-sso/src/main/java/cn/coderap/controller/HelloController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
 import javax.annotation.Resource;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -25,6 +26,11 @@ public class HelloController {
 
     private final static Logger logger= LoggerFactory.getLogger(HelloController.class);
     public static final String USER_TOKEN_REDIS = "user_token";
+
+    public static final String USER_TICKET_REDIS = "user_ticket";
+    public static final String USER_TICKET_COOKIE = "user_ticket_cookie";
+
+    public static final String TMP_TICKET_REDIS = "tmp_ticket";
 
     @Resource
     private UserService userService;
@@ -44,6 +50,20 @@ public class HelloController {
         return "login";
     }
 
+    /**
+     * CAS的统一登录接口
+     * 1、登录后创建用户的全局会话uniqueToken
+     * 2、创建用户全局门票userTicket，用以表示在CAS端是否登录过
+     * 3、创建用户的临时票据tmpTicket，用以回跳时回传
+     * @param username
+     * @param password
+     * @param returnUrl
+     * @param model
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/doLogin")
     public String login(String username,
                             String password,
@@ -61,18 +81,51 @@ public class HelloController {
             return "login";
         }
 
+        //1、实现登录
         Users userRes = userService.queryUserForLogin(username, MD5Utils.getMD5Str(password));
         if (userRes==null) {
             model.addAttribute("errmsg","用户名或密码不正确");
             return "login";
         }
 
-        //创建用户会话
+        //2、创建用户的redis会话
         String uniqueToken = UUID.randomUUID().toString().trim();
         UsersVO usersVO = new UsersVO();
         BeanUtils.copyProperties(userRes, usersVO);
         usersVO.setUserUniqueToken(uniqueToken);
         redisOperator.set(USER_TOKEN_REDIS + ":" + userRes.getId(), JsonUtils.objectToJson(usersVO));
+
+        //3、生成全局门票userTicket，代表用户在CAS端登录过
+        String userTicket = UUID.randomUUID().toString().trim();
+        //3.1、用户全局门票userTicket需要放到CAS端的cookie中(刚开始没有太理解！！！)
+        setCookie(USER_TICKET_COOKIE,userTicket,response); //TODO 可以对userTicket进行base64加密
+        //4、全局门票userTicket关联用户id,并放到到redis中（以动物园为例，表示这个用户有门票了，可以在各个景区游玩）
+        redisOperator.set(USER_TICKET_REDIS + ":" + userTicket, userRes.getId());
+        //5、生成临时票据tmpTicket（由CAS端签发的一个一次性的临时ticket），然后携带tmpTicket回跳到sso-mtv网站
+        String tmpTicket = createTmpTicket();
         return "login";
+//        return "redirect:" + returnUrl + "?tmpTicket=" + tmpTicket;
+    }
+
+    /**
+     * 创建临时票据
+     * @return
+     */
+    private String createTmpTicket() {
+        String tmpTicket = UUID.randomUUID().toString().trim();
+        //将临时票据保存到redis
+        try {
+            redisOperator.set(TMP_TICKET_REDIS + ":" + tmpTicket, MD5Utils.getMD5Str(tmpTicket),600);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return tmpTicket;
+    }
+
+    private void setCookie(String key,String val,HttpServletResponse response) {
+        Cookie cookie = new Cookie(key, val);
+        cookie.setDomain("sso.com");
+        cookie.setPath("/");
+        response.addCookie(cookie);
     }
 }
